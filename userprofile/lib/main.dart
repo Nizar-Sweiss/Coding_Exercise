1a20d174e48e691ed3193ed656ece0c64dc90d4b
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-12-03 13:29:08 +0300

    style: Theme Data Style
    Defines the configuration of the overall visual Theme for a MaterialApp or a widget subtree within the app

--- userprofile/lib/main.dart
+++ userprofile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:userprofile/screens/screens_barrel.dart';
+import 'package:userprofile/style/style_barrel.dart';
 import 'package:userprofile/utility/utility_barrel.dart';
 import 'utility/firebase_options.dart';
 
@@ -15,7 +16,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: light_blue_tint_2,
+              iconSize: 40,
+              splashColor: light_blue_tint_2),
+          colorSchemeSeed: oxford_blue_tint_2,
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: oxford_blue_tint_2,
+          textTheme: const TextTheme(
+            headline1: TextStyle(
+                color: light_blue_tint_2,
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+            headline2: TextStyle(
+              color: light_blue_tint_2,
+              fontSize: 25,
+            ),
+            headline3: TextStyle(
+                color: light_blue_tint_2,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+            bodyText2: TextStyle(color: oxford_blue_tint_1),
+          )),
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
       home: ProfileInfoScreen(),
