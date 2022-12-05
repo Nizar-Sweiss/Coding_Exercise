a56705a414437dd3b99e09c78e7f3d0347942152
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-12-05 14:33:18 +0300

    fix: remove unused imports

--- userprofile/lib/utility/firebase/storage_service.dart
+++ userprofile/lib/utility/firebase/storage_service.dart
@@ -1,21 +1,15 @@
 import 'dart:io';
-
-import 'package:firebase_storage/firebase_storage.dart ' as firebase_storage;
-import 'package:firebase_core/firebase_core.dart' as firebase_core;
-import 'package:flutter/material.dart';
-
-import '../utility_barrel.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Storage {
-  final firebase_storage.FirebaseStorage storage =
-      firebase_storage.FirebaseStorage.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
 
   Future<void> uploadFile(String filePath, String fileName) async {
     File file = File(filePath);
 
     try {
       await storage.ref('$fileName').putFile(file);
-    } on firebase_core.FirebaseException catch (e) {
+    } on FirebaseException catch (e) {
       print(e);
     }
   }
