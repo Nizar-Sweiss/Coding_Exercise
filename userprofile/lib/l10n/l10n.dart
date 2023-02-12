11bbdb937e30153e3c7e2a345fdc3139b8234ccc
Nizar Sweiss <Nizarsweiss00@gmail.com>
2023-02-12 19:01:20 +0300

    refactor: main and l10n

--- userprofile/lib/l10n/l10n.dart
+++ userprofile/lib/l10n/l10n.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class L10n {
   static final all = [
     const Locale("en"),
     const Locale("ar"),
   ];
+
+  static List<LocalizationsDelegate<dynamic>> get localizationsDelegates {
+    return [
+      AppLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate
+    ];
+  }
 }
--- userprofile/lib/main.dart
+++ userprofile/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:userprofile/l10n/l10n.dart';
-import 'package:userprofile/screens/screens_barrel.dart';
 import 'package:userprofile/style/app_style.dart';
 import 'package:userprofile/utility/utility_barrel.dart';
 import 'utility/firebase/firebase_options.dart';
-import 'package:flutter_gen/gen_l10n/app_localization.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,15 +20,10 @@ class MyApp extends StatelessWidget {
       scaffoldMessengerKey: Utils.messengerKey,
       theme: appTheme(),
       supportedLocales: L10n.all,
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
+      localizationsDelegates: L10n.localizationsDelegates,
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
-      home: ProfileInfoScreen(),
+      home: RouteGenerator.launchHomeScreen(),
     );
   }
 }
