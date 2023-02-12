27dc9c0e155bdfd42decfe29677b5b1af2c6967f
Nizar Sweiss <Nizarsweiss00@gmail.com>
2023-02-12 18:49:50 +0300

    refactor: route_generator class

--- userprofile/lib/utility/route_generator.dart
+++ userprofile/lib/utility/route_generator.dart
@@ -1,42 +1,53 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:userprofile/models/user.dart';
+import 'package:userprofile/screens/error_route_screen.dart';
 import 'package:userprofile/screens/screens_barrel.dart';
 
 class RouteGenerator {
+  static RouteSettings routeSettings = const RouteSettings();
+  static Widget homeScreen = const ProfileInfoScreen();
+
+  static launchHomeScreen() {
+    return RouteGenerator.homeScreen;
+  }
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
+    setRouteSettings(settings);
+    return navigateToPage();
+  }
+
+  static Route<dynamic> navigateToPage() {
+    switch (routeSettings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const ProfileInfoScreen());
-      case '/editProfile':
-        User userData = settings.arguments as User;
-
-        return MaterialPageRoute(
-            builder: (_) => EditProfileScreen(
-                  userData: (userData),
-                ));
-
+        return homePage();
+      case '/editPrsssofile':
+        return editProfileScreenPage();
       default:
-        return _errorRoute();
+        return _errorRoutePage();
     }
   }
 
-  static Route<dynamic> _errorRoute() {
+  static void setRouteSettings(RouteSettings settings) {
+    routeSettings = settings;
+  }
+
+  static MaterialPageRoute<dynamic> editProfileScreenPage() {
+    User userData = routeSettings.arguments as User;
+
+    return MaterialPageRoute(
+      builder: (_) => EditProfileScreen(
+        userData: (userData),
+      ),
+    );
+  }
+
+  static MaterialPageRoute<dynamic> homePage() =>
+      MaterialPageRoute(builder: (_) => RouteGenerator.launchHomeScreen());
+
+  static Route<dynamic> _errorRoutePage() {
     return MaterialPageRoute(
       builder: (_) {
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Color.fromARGB(255, 135, 7, 7),
-          ),
-          body: const Center(
-            child: Icon(
-              Icons.error,
-              size: 100,
-              color: Color.fromARGB(255, 255, 255, 255),
-            ),
-          ),
-        );
+        return const ErrorRouteScreen();
       },
     );
   }
