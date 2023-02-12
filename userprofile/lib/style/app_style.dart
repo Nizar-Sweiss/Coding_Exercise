845403d4c33ffac23663f0aa5496e88ef35f5848
Nizar Sweiss <Nizarsweiss00@gmail.com>
2023-02-12 18:50:48 +0300

    add colors into app_style

--- userprofile/lib/style/app_style.dart
+++ userprofile/lib/style/app_style.dart
@@ -2,30 +2,31 @@
 
 import 'package:flutter/material.dart';
 
-const oxford_blue_tint_1 = Color.fromARGB(255, 43, 52, 69); //2B3445
-const oxford_blue_tint_2 = Color.fromARGB(255, 48, 58, 76); //303A4C
-const oxford_blue_tint_3 = Color.fromARGB(255, 76, 92, 111); //4C5C6F
-const oxford_blue_tint_4 = Color.fromARGB(255, 66, 84, 102); //425466
-const oxford_blue_tint_5 = Color.fromARGB(255, 79, 92, 111); //4F5C6F
+const oxford_blue_tint_1 = Color.fromARGB(255, 43, 52, 69);
+const oxford_blue_tint_2 = Color.fromARGB(255, 48, 58, 76);
+const oxford_blue_tint_3 = Color.fromARGB(255, 76, 92, 111);
+const oxford_blue_tint_4 = Color.fromARGB(255, 66, 84, 102);
+const oxford_blue_tint_5 = Color.fromARGB(255, 79, 92, 111);
 
-const blue_tint_1 = Color.fromARGB(255, 76, 121, 255); //4C79FF
-const blue_tint_2 = Color.fromARGB(255, 134, 154, 233); //869AE9
+const blue_tint_1 = Color.fromARGB(255, 76, 121, 255);
+const blue_tint_2 = Color.fromARGB(255, 134, 154, 233);
 
-const red_tint_1 = Color.fromARGB(255, 255, 74, 74); //FF4A4A
-const red_tint_2 = Color.fromARGB(255, 255, 186, 186); //FFBABA
+const red_error = Color.fromARGB(255, 255, 15, 15);
+const red_tint_1 = Color.fromARGB(255, 255, 74, 74);
+const red_tint_2 = Color.fromARGB(255, 255, 186, 186);
 
-const light_blue_tint_1 = Color.fromARGB(255, 123, 143, 177); //7B8FB1
-const light_blue_tint_2 = Color.fromARGB(255, 236, 242, 247); // ECF2F7
+const light_blue_tint_1 = Color.fromARGB(255, 123, 143, 177);
+const light_blue_tint_2 = Color.fromARGB(255, 236, 242, 247);
 
-const light_grey_tint_1 = Color.fromARGB(255, 184, 193, 202); //B8C1E0
+const light_grey_tint_1 = Color.fromARGB(255, 184, 193, 202);
 
-const light_green_tint_1 = Color.fromARGB(255, 210, 255, 206); //D2FFCE
-const light_yellow_tint_2 = Color.fromARGB(255, 250, 255, 190); //FAFFBE
+const light_green_tint_1 = Color.fromARGB(255, 210, 255, 206);
+const light_yellow_tint_2 = Color.fromARGB(255, 250, 255, 190);
 const light_green_tint_2 = Color.fromARGB(255, 14, 170, 0);
 
 appTheme() {
   return ThemeData(
-      iconTheme: IconThemeData(
+      iconTheme: const IconThemeData(
         color: light_blue_tint_2,
       ),
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
